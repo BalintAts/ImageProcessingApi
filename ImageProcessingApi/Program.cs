86f726d5bbe4c8b2ecc0dbf258f7e3532e617dac
atsbalint <atsbalint@DESKTOP-72HFBIG>
2025-11-13 16:19:38 +0100

    Configure ProblemsDetails with Detail = exception message

--- ImageProcessingApi/Program.cs
+++ ImageProcessingApi/Program.cs
@@ -1,67 +1,53 @@
 
+using System;
 using ImageProcessingApi.Logic;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
-namespace ImageProcessingApi
+namespace ImageProcessingApi;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddProblemDetails(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddProblemDetails();
-
-            builder.Services.AddScoped<ImageFileProcessor, ImageFileProcessor>();
-
-
-            var app = builder.Build();
-
-            app.UseExceptionHandler(appError =>
+            options.CustomizeProblemDetails = context =>
             {
-                appError.Run(async context =>
+                var exception = context.HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+                if (exception is not null)
                 {
-                    var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+                    context.ProblemDetails.Detail = exception.Message;
+                }
+            };
+        });
 
-                    var problem = new ProblemDetails
-                    {
-                        Title = "An unexpected error occurred",
-                        Status = 500,
-                        Detail = exception?.Message,
-                        Instance = context.Request.Path,
-                        Type = "https://example.com/problems/internal-server-error"
-                    };
+        builder.Services.AddScoped<ImageFileProcessor, ImageFileProcessor>();
 
-                    problem.Extensions["traceId"] = context.TraceIdentifier;
+        var app = builder.Build();
 
-                    context.Response.StatusCode = 500;
-                    context.Response.ContentType = "application/problem+json";
-                    await context.Response.WriteAsJsonAsync(problem);
-                });
-            });
+        app.UseExceptionHandler();
 
-            app.UseStatusCodePages();
+        app.UseStatusCodePages();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
